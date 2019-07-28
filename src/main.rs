c56a8ea4f2bb37e2bb5f1d6230a91296628bc89f
Ronja Koistinen <ronja.koistinen@kapsi.fi>
2019-07-28 18:25:53 +0300

    Working player movement

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,11 @@
 use rand::prelude::*;
 use sdl2::event::Event;
-use sdl2::keyboard::Keycode;
+use sdl2::image::{InitFlag, LoadTexture};
+use sdl2::keyboard::{Keycode, Scancode};
 use sdl2::pixels::Color;
-use sdl2::rect::Point;
+use sdl2::rect::{Point, Rect};
 use std::cmp;
+use std::collections::HashSet;
 use std::time::Duration;
 
 #[derive(Clone, Copy)]
@@ -14,9 +16,78 @@ struct Star {
     pub y: i32,
 }
 
+#[derive(Debug, Clone, Copy)]
+struct Velocity {
+    pub x: i32,
+    pub y: i32,
+}
+
+enum Axis {
+    X,
+    Y,
+}
+
+impl Velocity {
+    pub fn new(x: i32, y: i32) -> Velocity {
+        Velocity {
+            x: x,
+            y: y,
+        }
+    }
+
+    pub fn apply_inertia(&mut self, axis: Axis) {
+        const INERTIA: i32 = 3;
+
+        match axis {
+            Axis::Y => {
+                if self.y == -1 {
+                    self.y = 0;
+                } else if self.y < 0 {
+                    self.y += INERTIA;
+                } else if self.y == 1 {
+                    self.y = 0;
+                } else if self.y > 0 {
+                    self.y -= INERTIA;
+                }
+            },
+            Axis::X => {
+                if self.x == -1 {
+                    self.x = 0;
+                } else if self.x < 0 {
+                    self.x += INERTIA;
+                } else if self.x == 1 {
+                    self.x = 0;
+                } else if self.x > 0 {
+                    self.x -= INERTIA;
+                }
+            }
+        }
+    }
+}
+
+impl std::ops::Add for Velocity {
+    type Output = Self;
+    fn add(self, other: Self) -> Velocity {
+        Velocity {
+            x: self.x + other.x,
+            y: self.y + other.y,
+        }
+    }
+}
+
+impl std::ops::AddAssign for Velocity {
+    fn add_assign(&mut self, other: Self) {
+        *self = Self {
+            x: self.x + other.x,
+            y: self.y + other.y,
+        }
+    }
+}
+
 const HEIGHT: u32 = 384;
 const WIDTH: u32 = 512;
 const MAX_STARS: usize = 128;
+const PLAYER_MAX_SPEED: i32 = 6;
 
 // How many free star slots are there?
 fn count_free_stars(stars: &[Option<Star>]) -> u32 {
@@ -88,6 +159,75 @@ fn advance_stars(stars: &mut [Option<Star>]) -> () {
     }
 }
 
+fn handle_input(keycodes: &HashSet<Keycode>, v: &mut Velocity) {
+    const SPEED: i32 = 2;
+    if !(keycodes.contains(&Keycode::Up) || keycodes.contains(&Keycode::Down)) {
+        v.apply_inertia(Axis::Y)
+    }
+    if !(keycodes.contains(&Keycode::Left) || keycodes.contains(&Keycode::Right)) {
+        v.apply_inertia(Axis::X)
+    }
+
+    for k in keycodes {
+        *v +=  match k {
+            Keycode::Up    => Velocity::new(0, -SPEED),
+            Keycode::Down  => Velocity::new(0, SPEED),
+            Keycode::Left  => Velocity::new(-SPEED, 0),
+            Keycode::Right => Velocity::new(SPEED, 0),
+            _ => Velocity::new(0, 0),
+        };
+    }
+
+    // limit player ship's maximum speed
+    if v.x > PLAYER_MAX_SPEED {
+        v.x = PLAYER_MAX_SPEED;
+    } else if v.x < -PLAYER_MAX_SPEED {
+        v.x = -PLAYER_MAX_SPEED;
+    }
+    if v.y > PLAYER_MAX_SPEED {
+        v.y = PLAYER_MAX_SPEED;
+    } else if v.y < -PLAYER_MAX_SPEED {
+        v.y = -PLAYER_MAX_SPEED;
+    }
+
+}
+fn move_player(player_ship: &sdl2::render::Texture, x: &mut i32, y: &mut i32, v: &mut Velocity) -> () {
+    let player_w = player_ship.query().width;
+    let player_h = player_ship.query().height;
+    let max_x = (WIDTH - 1 - player_w) as i32 - v.x;
+    let max_y = (HEIGHT - 1 - player_h) as i32 - v.y;
+
+    if *x >= 0 && *x < max_x {
+        *x += v.x;
+    } else if *x < 0 {
+        *x = 0;
+        *v = Velocity::new(0, v.y);
+    } else if *x >= max_x {
+        *x = max_x - 1;
+        *v = Velocity::new(0, v.y);
+    }
+
+    if *y >= 0 && *y < max_y {
+        *y += v.y;
+    } else if *y < 0 {
+        *y = 0;
+        *v = Velocity::new(v.x, 0);
+    } else if *y >= max_y {
+        *y = max_y - 1;
+        *v = Velocity::new(v.x, 0);
+    }
+}
+
+fn draw_player(
+    canvas: &mut sdl2::render::Canvas<sdl2::video::Window>,
+    player_ship: &sdl2::render::Texture,
+    x: i32,
+    y: i32,
+) -> Result<(), String> {
+    let rect = Rect::new(x, y, player_ship.query().width, player_ship.query().height);
+    canvas.copy(player_ship, None, rect)
+}
+
 fn main() -> Result<(), String> {
     let sdl_context = sdl2::init().unwrap();
     let video = sdl_context.video().unwrap();
@@ -102,15 +242,25 @@ fn main() -> Result<(), String> {
     let mut canvas = window.into_canvas().build().unwrap();
     canvas.set_scale(2.0, 2.0).unwrap();
 
+    let _image_context = sdl2::image::init(InitFlag::PNG)?;
+    let texture_creator = canvas.texture_creator();
+
     // A buffer of Option<Star>
     // If an element is None, that slot in the buffer is available for
     // spawning a new star.
     let mut stars: [Option<Star>; MAX_STARS] = [None; MAX_STARS];
 
+    let mut player_x: i32 = WIDTH as i32 / 5;
+    let mut player_y: i32 = WIDTH as i32 / 2;
+    let mut player_v: Velocity = Velocity::new(0, 0);
+    // image binaries currently not in source control
+    let player_ship = texture_creator.load_texture("playership.png")?;
+
     // for the first frame we need to draw the star field differently
     let mut first_frame = true;
 
     let mut event_pump = sdl_context.event_pump().unwrap();
+
     'running: loop {
         // Blank the window
         canvas.set_draw_color(Color::RGB(0, 0, 0));
@@ -125,6 +275,16 @@ fn main() -> Result<(), String> {
                 _ => {}
             }
         }
+
+        let pressed_keys: HashSet<_> = event_pump
+            .keyboard_state()
+            .pressed_scancodes()
+            .filter_map(Keycode::from_scancode)
+            .collect();
+
+        handle_input(&pressed_keys, &mut player_v);
+        move_player(&player_ship, &mut player_x, &mut player_y, &mut player_v);
+
         spawn_new_stars(&mut stars, first_frame);
 
         for s in stars.iter().filter_map(|&x| x) {
@@ -134,6 +294,7 @@ fn main() -> Result<(), String> {
                 .or(Err("Unable to draw star\n"))?;
         }
         advance_stars(&mut stars);
+        draw_player(&mut canvas, &player_ship, player_x, player_y)?;
         canvas.present();
         ::std::thread::sleep(Duration::new(0, 1_000_000_000u32 / 60));
         first_frame = false;
